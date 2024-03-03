f7adb2b734c02ca9c85efe3190b27730180a24e0
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-03 12:26:05 +0100

    lsp updates

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,47 +1,66 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
--- see :help lsp-zero-keybindings
--- to learn the available actions
-lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-    -- Replace the language servers listed here 
-    -- with the ones you want to install
-    ensure_installed = {
-      -- lsp
-      'rust_analyzer',
-      'lua_ls',
-      'bashls',
-      'pyright',
-      'tsserver',
-      'clangd',
-      -- -- formatters
-      -- 'black',
-      -- 'isort',
-      -- 'prettier',
-      -- 'stylua',
-      -- 'clang-format',
-      -- -- linters
-      -- 'eslint_d',
-      -- 'ruff',
-    },
-    handlers = {
-      lsp_zero.default_setup,
-    },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	-- Replace the language servers listed here
+	-- with the ones you want to install
+	ensure_installed = {
+		-- lsp
+		"rust_analyzer",
+		"lua_ls",
+		"bashls",
+		"pyright",
+		"tsserver",
+		"clangd",
+	},
+	handlers = {
+		lsp_zero.default_setup,
+	},
 })
 
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
-lspconfig.pyright.setup {}
-lspconfig.tsserver.setup {}
-lspconfig.rust_analyzer.setup {
-  -- Server-specific settings. See `:help lspconfig-setup`
-  settings = {
-    ['rust-analyzer'] = {},
-  },
-}
-lspconfig.clangd.setup {}
+lspconfig.pyright.setup({})
+lspconfig.tsserver.setup({})
+lspconfig.rust_analyzer.setup({
+	-- Server-specific settings. See `:help lspconfig-setup`
+	settings = {
+		["rust-analyzer"] = {},
+	},
+})
+lspconfig.clangd.setup({})
 
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		-- LSP
+		"rust_analyzer",
+		"lua_ls",
+		"bashls",
+		"pyright",
+		"tsserver",
+		"clangd",
+		"bash-language-server",
+		"lua-language-server",
+		"vim-language-server",
+		-- linters / formatters
+		"golangci-lint",
+		"stylua",
+		"shellcheck",
+		"editorconfig-checker",
+		"shellcheck",
+		"shfmt",
+		"yamlfmt",
+		"yamlls",
+		"sqlls",
+		"prettier",
+		"stylua",
+		"clang-format",
+		"eslint_d",
+	},
+})
