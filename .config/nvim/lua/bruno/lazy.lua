a857c6dd4b6cc98e2277af5501563484aba684c8
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2025-06-21 22:31:50 +0200

    update: mini files and mappings

--- .config/nvim/lua/bruno/lazy.lua
+++ .config/nvim/lua/bruno/lazy.lua
@@ -35,6 +35,8 @@ return require("lazy").setup({
 		event = "BufReadPre",
 		opts = {
 			dir = vim.fn.stdpath("state") .. "/sessions/",
+			need = 1,
+			branch = true, -- use git branch to save session
 		},
 	},
 
